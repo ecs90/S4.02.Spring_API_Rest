a4e952e65399761ed4068f9cb87fd21d400bec97
ecs90 <ecs_salas@hotmail.com>
2024-02-15 12:33:16 +0100

    first commit

---
+++ S04T02N01SalasDeramoEzequiel/src/main/java/cat/itacademy/barcelonactiva/salasderamo/ezequiel/s04/t02/n01/S04T02N01SalasDeramoEzequielApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.salasderamo.ezequiel.s04.t02.n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T02N01SalasDeramoEzequielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T02N01SalasDeramoEzequielApplication.class, args);
+	}
+
+}
